e8f6766a7dde128f44e5afa21f887c6c5bd0b33d
Mauro Agrelo <maurogrelo78@gmail.com>
2021-05-24 14:05:52 -0300

    prueba 2

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -5,8 +5,7 @@ function obtenerMayor(x, y) {
   // Devuelve el número más grande
   // Si son iguales, devuelve cualquiera de los dos
   // Tu código:
-  var hola =1
-  return 3 
+
 }
 
 function mayoriaDeEdad(edad) {
